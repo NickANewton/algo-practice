b5a9358e6bb350459fdb12f68a6077c51050392d
Nick Newton <nickanewton@gmail.com>
2022-07-19 12:49:28 -0700

    Added hash map solution.

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -9,3 +9,14 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+var twoSum = function(nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in map) {
+      return [map[target - nums[i], i]]
+    } else {
+      map[nums[i]] = i;
+    }
+  }
+}
